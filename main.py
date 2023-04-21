6ca16ad6c369040fdfe8be36a5e84bf88028e887
1antonioleite1 <128155482+1antonioleite1@users.noreply.github.com>
2023-04-20 21:21:23 -0300

    feito

---
+++ main.py
@@ -0,0 +1,16 @@
+numero = input("digite um número inteiro")
+numero = int(numero)
+contador = 0
+a = 2
+while contador != numero:
+    b = 2
+    primo = True
+    while b < a:
+        if a % b == 0 and a != b:
+            primo = False
+            break
+        b = b + 1
+    if primo:
+        contador += 1
+        print(a)
+    a = a + 1
